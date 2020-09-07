b6017fb9a16d50bae5179a4b656a0b9eda36ce95
刘大卫 <liudawei@iothinking.com>
2020-09-07 22:26:39 +0800

    完成控制中心方向界面设计

---
+++ app/src/main/java/com/example/zksoftware/MyApplication.java
@@ -0,0 +1,18 @@
+package com.example.zksoftware;
+
+import android.app.Application;
+
+import com.xuexiang.xui.XUI;
+
+/**
+ * Created by looper on 2020/9/7.
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        XUI.init(this); //初始化UI框架
+        XUI.debug(true);  //开启UI框架调试日志
+    }
+}
