6da3ccbe47b06677ba1612a4dffe5d8ae8ae11af
刘大卫 <liudawei@iothinking.com>
2020-09-12 09:35:55 +0800

    部分控件初始化

--- app/src/main/java/com/example/zksoftware/MainActivity.java
+++ app/src/main/java/com/example/zksoftware/MainActivity.java
@@ -3,16 +3,134 @@ package com.example.zksoftware;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
+    /**
+     * 四台监控
+     */
+    private SurfaceView camera1;
+    private SurfaceView camera2;
+    private SurfaceView camera3;
+    private SurfaceView camera4;
+    private SurfaceHolder holder1;
+    private SurfaceHolder holder2;
+    private SurfaceHolder holder3;
+    private SurfaceHolder holder4;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+
+        findId();
+
+        // 初始化holder
+        initSurfaceHolder();
+        // 初始化控件
+        initWidget();
     }
+
+
+    /**
+     * 初始化SurfaceHolder回调
+     */
+    private void initSurfaceHolder()
+    {
+        holder1 = camera1.getHolder();
+        holder1.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
+
+
+        holder2 = camera2.getHolder();
+        holder2.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
+
+        holder3 = camera3.getHolder();
+        holder3.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
+
+        holder4 = camera4.getHolder();
+        holder4.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
+
+    }
+
+
+    private void findId()
+    {
+        camera1 = findViewById(R.id.sv_camera1);
+        camera2 = findViewById(R.id.sv_camera2);
+        camera3 = findViewById(R.id.sv_camera3);
+        camera4 = findViewById(R.id.sv_camera4);
+    }
+
+
+    private void initWidget()
+    {
+
+    }
+
 }
