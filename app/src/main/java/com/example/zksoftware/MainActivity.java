a5d51425503cb73b8bb11adbd23f610dec3cfa8c
刘大卫 <liudawei@iothinking.com>
2020-09-14 22:39:56 +0800

    去除无用代码

--- app/src/main/java/com/example/zksoftware/MainActivity.java
+++ app/src/main/java/com/example/zksoftware/MainActivity.java
@@ -9,7 +9,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
